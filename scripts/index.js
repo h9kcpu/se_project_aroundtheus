037c221192a524f1029e84d46ab8e2d79be00ae3
Sean Watson <sean.watson49@yahoo.com>
2022-06-08 12:36:04 -0400

    console error

--- scripts/index.js
+++ scripts/index.js
@@ -1,50 +1,56 @@
-/* const initialCards = [
+const initialCards = [
     {
-        name: 'Yosemite Valley'
-        link: 'https://code.s3.yandex.net/web-code/yosemite.jng'
-
-        name: 'Lake Louise'
-        link: 'https://code.s3.yandex.net/web-code/lake-louise.jng'
-
-        name: 'Bald Mountains'
-        link: 'https://code.s3.yandex.net/web-code/bald-mountains.jng'
-
-        name: 'Latemar'
-        link: 'https://code.s3.yandex.net/web-code/latemar.jng'
-
-        name: 'Vanoise'
-        link: 'https://code.s3.yandex.net/web-code/vanoise.jng'
-
-        name: 'Lago di Braies'
-        link: 'https://code.s3.yandex.net/web-code/lago.jng'
-    }
+        name: 'Yosemite Valley',
+        link: 'https://code.s3.yandex.net/web-code/yosemite.jpg',
+    },
+    {
+        name: 'Lake Louise',
+        link: 'https://code.s3.yandex.net/web-code/lake-louise.jpg',
+    },
+    {
+        name: 'Bald Mountains',
+        link: 'https://code.s3.yandex.net/web-code/bald-mountains.jpg',
+    },
+    {
+        name: 'Latemar',
+        link: 'https://code.s3.yandex.net/web-code/latemar.jng',
+    },
+    {
+        name: 'Vanoise National Park',
+        link: 'https://code.s3.yandex.net/web-code/vanoise.jng',
+    },
+    {
+        name: 'Lago di Braies',
+        link: 'https://code.s3.yandex.net/web-code/lago.jng',
+    },
 ]
 
 const cardTemplate = document
     .querySelector('#card-template')
-    .textContent.querySelector(.'card');
+    .content.querySelector('.card');
 
 const cardsWrap = document.querySelector('.cards__list');
 const modal = document.querySelector('.modal');
 const profileFormElement = document.querySelector('.modal__form');
 
 const profileEditButton = document.querySelector('.profile__button-edit');
-const modalCloseButton = decument.querySelector('.modal__close');
+const modalCloseButton = document.querySelector('.modal__close');
 const profileTitle = document.querySelector('.profile__info-title');
 const profileDescription = document.querySelector('.profile__info-description');
 
-const nameInput = profileFormElement.querySelector('.modal__input_type_name');
-const jobInput = profileFormElement.querySelector(.'modal__input_type_description');
+const nameInput = profileFormElement.querySelector('.modal__input-name');
+const jobInput = profileFormElement.querySelector('.modal__input-description');
 
 function closeModal() {
-    modal.classList.remove('modal_is-opened');
+    modal.style.display = 'none';
 }
 
 function openModal() {
     nameInput.value = profileTitle.textContent;
     jobInput.value = profileDescription.textContent;
 
-    modal.classList.add('modal_list-opened');
+    modal.style.display = 'flex';
+
 }
 
 function handleProfileFormSubmit(evt) {
@@ -68,10 +74,11 @@ function getCardElement(data) {
     return cardElement;
 }
 
-profileFormElement.addEventListener('submit' , handleProfileFormSubmit) ;
-profileEditButton.addEventListener('click' , openModal);
-modalCloseButton.addEventListener('click' , closeModal);
+profileFormElement.addEventListener('submit', handleProfileFormSubmit);
+profileEditButton.addEventListener('click', openModal);
+modalCloseButton.addEventListener('click', closeModal);
+
 
 for (let i = 0; i < initialCards.length; i++) {
-    cardsWrap.prepend(getCardElement(initalCards[i]));-
-}
+    cardsWrap.prepend(getCardElement(initialCards[i]));
+} 
